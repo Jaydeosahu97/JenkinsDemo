aa9fe71c1fe84bd5627ed5f5cd967e0fdc659f01
Jaydeo Sahu <sahujaydeo@gmail.com>
2022-10-30 14:57:07 +0530

    initial commit

---
+++ src/test/java/com/dojo/MyLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
